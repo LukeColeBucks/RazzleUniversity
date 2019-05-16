900beb5a691dbdec8b282f12593dd4543f9bffbf
Luke Cole <luke.cole@sky.uk>
2019-05-16 09:04:19 +0100

    Adjust uri to sort by technology topics and popularity of articles

--- app/news/src/News.js
+++ app/news/src/News.js
@@ -1,5 +1,5 @@
 const url =
-"https://newsapi.org/v2/top-headlines?country=us&apiKey=54a8f685019f473ab5c0e306ce2d4403";
+"https://newsapi.org/v2/everything?q=tech&from=2019-05-16&sortBy=popularity&apiKey=54a8f685019f473ab5c0e306ce2d4403";
 
 export async function getNews() {
     let result = await fetch(url).then(response => response.json());
