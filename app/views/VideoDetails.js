71a17b429c93add9dc579d3368608d618ca21346
Luke Cole <luke.cole@sky.uk>
2019-06-15 13:44:47 +0100

    Include video page to view - BUG present, scale to screen size later on

--- app/views/VideoDetails.js
+++ app/views/VideoDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, WebView } from 'react-native';
 
 export class VideoDetail extends React.Component {
     static navigationOptions = {
@@ -8,10 +8,16 @@ export class VideoDetail extends React.Component {
 
     render() {
         let tubeId = this.props.navigation.getParam('ytubeId', 'NO VIDEO');
+        let tubeUrl = `https://www.youtube.com/embed/${tubeId}`;
         return(
-            <View style = {{ paddingTop: 40}}>
-                <Text>{ tubeId }</Text>
-            </View>
+            <WebView 
+                style = {{ marginTop: 20 }}
+                javaScriptEnabled = { true }
+                source = {{ uri: tubeUrl }}
+            />
+            // <View style = {{ paddingTop: 40}}>
+            //     <Text>{ tubeId }</Text>
+            // </View> only for testing to display tubeID if video fails, debug with console.
         );
     }
 }
